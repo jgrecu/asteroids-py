3b2e50f3012665b6a6f5cd282cff7936d09d605b
Jeremy Grecu <jgrecu@gmail.com>
2025-12-27 10:28:39 +0100

    Add Player class, render the initial position for the player on the screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -13,6 +14,8 @@ def main():
     pyclock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
 
@@ -21,6 +24,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = pyclock.tick(60) / 1000
 
