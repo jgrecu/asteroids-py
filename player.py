009b842bdaaaaaa8d44a9643312e237cc3de8242
Jeremy Grecu <jgrecu@gmail.com>
2026-01-03 10:42:43 +0100

    Fix the continuous shot

--- player.py
+++ player.py
@@ -3,7 +3,8 @@ from string import whitespace
 import pygame
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, \
+    PLAYER_SHOOT_COOLDOWN_SECONDS
 from shot import Shot
 
 
@@ -11,6 +12,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -27,6 +29,7 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+        self.shot_cooldown_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -51,6 +54,8 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
 
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y, self.radius)
-        rotated_vector = pygame.Vector2(0, 1).rotate(self.rotation)
-        shot.velocity = rotated_vector * PLAYER_SHOOT_SPEED
+        if self.shot_cooldown_timer <= 0:
+            self.shot_cooldown_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
+            shot = Shot(self.position.x, self.position.y, self.radius)
+            rotated_vector = pygame.Vector2(0, 1).rotate(self.rotation)
+            shot.velocity = rotated_vector * PLAYER_SHOOT_SPEED
